baba65f96d8843251fe1872329689dc622576c21
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-01 23:31:55 -0400

    Update RentalsMap.js

--- src/components/RentalsMap.js
+++ src/components/RentalsMap.js
@@ -1,14 +1,41 @@
 import React from "react";
 import {Map, Marker, GoogleApiWrapper} from "google-maps-react"
+import {useState, useEffect} from "react";
 
-function RentalsMap({locations}) {
+function RentalsMap({locations, google}) {
+
+  const [center, setCenter] = useState();
+  useEffect(() => {
+    var arr = Object.keys(locations);
+    var getLat = (key) => locations[key]["lat"];
+    var avgLat = arr.reduce((a, c) => a + Number(getLat(c)), 0) / arr.length;
+
+    var getLng = (key) => locations[key]["lng"];
+    var avgLng = arr.reduce((a, c) => a + Number(getLng(c)), 0) / arr.length;
+
+    setCenter({lat: avgLat, lng: avgLng});
+  }, [locations]);
 
   return (
     <>
-      <div>Map</div>
+      {center && (
+        <Map
+          google={google}
+          containerStyle={{
+            width: "50vw",
+            height: "calc(100vh - 135px)",
+          }}
+          center={center}
+          initialCenter={locations[0]}
+          zoom={13}
+          disableDefaultUI={true}
+          ></Map>
+      )}
     </>
   );
 }
 
 // export default RentalsMap;
-export 
+export default GoogleApiWrapper({
+  apiKey: ""
+}) (RentalsMap);
